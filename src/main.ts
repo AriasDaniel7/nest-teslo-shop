98c2649cb5419be15f49c52a80c5d9aff88317b8
Daniel Arias <daniel7arias2003@gmail.com>
2024-08-12 21:58:37 -0500

    ver puerto

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Product } from './products/entities/product.entity';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -24,6 +25,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  console.log(process.env.PORT);
 
   await app.listen(process.env.PORT || 3000);
 }
